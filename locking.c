6ec367ef8bc67220c83ca9a27d0aeae72a66ff46
akhilyendluri <akhilyendluri@gmail.com>
2017-11-11 09:54:27 -0500

    Updating locking.c
    Final update

--- locking.c
+++ locking.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-//#include <sys/types.h>
-//#include <pthread.h>
 
 #include "locking.h"
 
@@ -12,9 +10,10 @@
 /* Exercise 1:
  *     Basic memory barrier
  */
+ 
 void mem_barrier() {
-    /* Implement this */
-    asm volatile("mfence":::"memory");
+   
+	asm volatile("mfence":::"memory");
 }
 
 
@@ -26,25 +25,28 @@ void
 atomic_sub( int * value,
 	    int   dec_val)
 {
-    /* Implement this */
-    int result;
+   int result;
 	asm volatile("lock; xaddl %0, %1"
 	: "=r" (result), "=m" (*value)
 	:"0"(-dec_val),"m"(*value)
 	:"memory"
 	);
+	
+	
+
 }
 
 void
 atomic_add( int * value,
 	    int   inc_val)
 {
-    /* Implement this */
-    asm volatile("lock; xaddl %0, %1"
+   asm volatile("lock; xaddl %0, %1"
 	: "+r" (inc_val), "+m" (*value)
 	:
 	:"memory"
 	);
+	
+
 }
 
 
@@ -63,40 +65,37 @@ compare_and_swap(unsigned int * ptr,
 		 unsigned int   expected,
 		 unsigned int   new)
 {
-    /* Implement this */
-    //unsigned char ret;
-    unsigned int original;
-    asm volatile (
-	"lock\n"
-	"cmpxchgl %1,%2\n"
-	: "=a" (original)
-	: "q" (new), "m" (*ptr), "0" (expected)
-	: "memory"
-    );
-    return original;
-}
+   unsigned int prev;
+	asm volatile(" lock\n"
+			"cmpxchgl %1,%2\n"			
+			: "=a" (prev)
+			: "q" (new), "m" (*ptr), "0" (expected)
+			: "memory");
 
+	return prev; 
+	
+
+}
 void
 spinlock_init(struct spinlock * lock)
 {
-    /* Implement this */
-    lock->free = 0;
+   lock->free=0;
 }
 
 void
 spinlock_lock(struct spinlock * lock)
 {
-    /* Implement this */
-    while(compare_and_swap(&lock->free,0,1)==1);
+  
+	while(compare_and_swap(&lock->free,0,1)==1);
+
 }
 
 
 void
 spinlock_unlock(struct spinlock * lock)
 {
-    /* Implement this */
-    lock->free = 0;
-    //printf("lock = %d",lock->free);
+   lock->free=0;	
+	
 }
 
 
@@ -105,8 +104,14 @@ int
 atomic_add_ret_prev(int * value,
 		    int   inc_val)
 {
-    /* Implement this */
-    return 0;
+	int prev;
+    	asm volatile("lock; xaddl %3, %4"
+	: "=a"(prev),"=r" (inc_val), "=m" (*value)
+	:"r"(inc_val),"m"(*value),"0"(*value)
+	:"memory"
+	);
+
+    return prev;
 }
 
 /* Exercise 4:
@@ -117,7 +122,6 @@ void
 barrier_init(struct barrier * bar,
 	     int              count)
 {
-    /* Implement this */
     bar->init_count = count;
     bar->iterations = 0;
 }
@@ -125,16 +129,27 @@ barrier_init(struct barrier * bar,
 void
 barrier_wait(struct barrier * bar)
 {
-    /* Implement this */
     atomic_add(&bar->cur_count,1);
-    if(bar->cur_count == bar->init_count) { mem_barrier(); bar->iterations = 1; }
-    while(bar->iterations != 1) {mem_barrier();};
+    
+    //when all thread have called barrier_wait(), make iterations = 1
+    if(bar->cur_count == bar->init_count) 
+      bar->iterations = 1;
+    
+    //wait until all threads have reached barrier
+    while(bar->iterations != 1);
+    
     atomic_sub(&bar->cur_count,1);    
+    
+    //last thread makes the iteration = 0
     if(bar->cur_count == 0) {
       bar->iterations = 0;
     }
-    while(bar->iterations != 0) { mem_barrier(); }
+    
+    //wait until last thread has reached this point, then all threads leave barrier
+    while(bar->iterations != 0);
 }
+
+
 
 
 /* Exercise 5:
@@ -144,47 +159,57 @@ barrier_wait(struct barrier * bar)
 void
 rw_lock_init(struct read_write_lock * lock)
 {
-    /* Implement this */
-    lock->num_readers=0;
-    lock->writer=0;
-    lock->mutex.free=0;
+   lock->num_readers=0;
+	lock->writer=0;
+	lock->mutex.free=0;
 }
 
 
 void
 rw_read_lock(struct read_write_lock * lock)
 {
-    /* Implement this */
-    while(1)
-    {
+   while(1)
+	{
+		//increment reader count
       atomic_add(&lock->num_readers,1);
-      if(!lock->mutex.free) return;
-      atomic_sub(&lock->num_readers,1);
-      while(lock->mutex.free);
-    }
+		
+      //check if lock is available
+      if(!lock->mutex.free)
+         return;
+         
+      //if lock not available, decrement the count       
+		atomic_sub(&lock->num_readers,1);
+     
+      //wait until lock is available
+		while(lock->mutex.free);
+	}
+
 }
 
 void
 rw_read_unlock(struct read_write_lock * lock)
 {
-    /* Implement this */
-    atomic_sub(&lock->num_readers,1);
+  //decrement reader count
+  atomic_sub(&lock->num_readers,1);
 }
 
 void
 rw_write_lock(struct read_write_lock * lock)
 {
-    /* Implement this */
-    spinlock_lock(&lock->mutex);
-    while(lock->num_readers);
+	//acquire lock
+   spinlock_lock(&lock->mutex);
+	
+   //watch for any reader
+   while(lock->num_readers);
+	
 }
 
 
 void
 rw_write_unlock(struct read_write_lock * lock)
 {
-    /* Implement this */
-    spinlock_unlock(&lock->mutex);
+   //release lock
+	spinlock_unlock(&lock->mutex);
 }
 
 
@@ -208,67 +233,78 @@ compare_and_swap_ptr(uintptr_t * ptr,
 		     uintptr_t   expected,
 		     uintptr_t   new)
 {
-    /* Implement this */
-//    struct node * t = (struct node *)ptr;
-//    struct node * t1 = (struct node *)expected;
-//    struct node * t2 = (struct node *)new;
-//    printf("cmp_swp enter ptr = %p\n",t->value);
-//    printf("new = %d",t2->value);
-    uintptr_t prev = 0;
-    asm volatile( "lock\n"
-		  "cmpxchgq %1,%2\n"
-		: "=a"(prev)
-		: "q"(new), "m"(*ptr), "0"(expected)
-		: "memory");
-//    t = ptr;
-//    printf("cmp_swp exit ptr = %p\n",t->value);
-    return prev ;
+	uintptr_t prev = 0;
+    	asm volatile( "lock\n"
+                  "cmpxchgq %1,%2\n"
+                : "=a"(prev)
+                : "q"(new), "m"(*ptr), "0"(expected)
+                : "memory");
+	return prev;
 }
 
+
+/*
+   inserts dummy node
+   makes head and tail point to the dummy node
+*/
 void
 lf_queue_init(struct lf_queue * queue)
 {
-    struct node * Node = malloc(sizeof(struct node));
-    Node->value = -1;
-    Node->next = NULL;
-    queue->head = Node;
-    queue->tail = Node;
+	struct node * Node = malloc(sizeof(struct node));
+    	Node->value = -1;
+    	Node->next = NULL;
+    	queue->head = Node;
+    	queue->tail = Node;
 }
+
 
 void
 lf_queue_deinit(struct lf_queue * lf)
 {
-    lf->head = NULL;
-    lf->tail = NULL;
+	   lf->head = NULL;
+    	lf->tail = NULL; 
 }
+
 
 void
 lf_enqueue(struct lf_queue * queue,
 	   int               val)
 {
-    struct node * Node = malloc(sizeof(struct node));
-    Node->value = val;
-    Node->next = NULL;
-    struct node * tail = queue->tail;
-    struct node * temp = tail;
-    do {
-	while(tail->next != NULL){ tail = tail->next; }
-    } while(!(compare_and_swap_ptr((uintptr_t *)&tail->next,(uintptr_t)NULL,(uintptr_t)Node) == (uintptr_t)NULL));
-    compare_and_swap_ptr((uintptr_t *)&queue->tail,(uintptr_t)temp,(uintptr_t)tail->next);        
+      //create a node
+    	struct node * Node = malloc(sizeof(struct node));
+    	Node->value = val;
+    	Node->next = NULL;
+    	struct node * tail = queue->tail;
+    	struct node * temp = tail;
+    	
+      //appending node to the tail atomically using compare_and_swap_ptr
+      do {
+         
+        	while(tail->next != NULL){
+					tail = tail->next;
+			}
+
+		} while((compare_and_swap_ptr((uintptr_t *)&tail->next,(uintptr_t)NULL,(uintptr_t)Node) != (uintptr_t)NULL));
+	
+   compare_and_swap_ptr((uintptr_t *)&queue->tail,(uintptr_t)temp,(uintptr_t)Node);
 }
 
 int
 lf_dequeue(struct lf_queue * queue,
-	   int             * val)
+		   int             * val)
 {
-    struct node * Node;
-    do {
-    	Node = queue->head;
-	if(Node->next == NULL) return 1;
-    }while(compare_and_swap_ptr((uintptr_t *)&queue->head,(uintptr_t)Node,(uintptr_t)Node->next) != (uintptr_t)Node);
-    *val = Node->next->value;
-    return 0;
+	struct node * Node;
+	
+   //removing node from head atomicallly using compare_and_swap_ptr
+   do {
+		Node = queue->head;
+		if(Node->next == NULL) 
+         return 1;
+	}while(compare_and_swap_ptr((uintptr_t *)&queue->head,(uintptr_t)Node,(uintptr_t)Node->next) != (uintptr_t)Node);
+	
+   *val = Node->next->value;
+	free(Node);
+   return 0;
 }
-
 
 
